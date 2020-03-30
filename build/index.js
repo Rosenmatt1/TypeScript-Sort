49c9af3d5be6ec69040fe1d122536212e8d77151
Matthew Rosenberg <Rosenmatt1@gmail.com>
2020-03-30 14:28:19 -0600

    Added build and src directories, and also added nodemon and concurrently

---
+++ build/index.js
@@ -0,0 +1,5 @@
+"use strict";
+console.log('hi there');
+var logSomething = function () {
+    console.log('something');
+};
---
+++ src/index.ts
@@ -0,0 +1,5 @@
+console.log('hi there')
+
+const logSomething = () => {
+  console.log('something')
+}
