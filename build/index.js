92797ac30da7f69b2e7d3b550fc32f8be67caa45
Matthew Rosenberg <Rosenmatt1@gmail.com>
2020-03-30 15:20:16 -0600

    Created bubble sort in sort class

--- build/index.js
+++ build/index.js
@@ -1,5 +1,28 @@
 "use strict";
-console.log('hi there');
-var logSomething = function () {
-    console.log('something');
-};
+var Sorter = /** @class */ (function () {
+    // collection: number[];
+    // constructor(collection: number[]) {
+    //   this.collection = collection;
+    // }
+    function Sorter(collection) {
+        this.collection = collection;
+    }
+    Sorter.prototype.sort = function () {
+        // const length = this.collection.length;
+        var length = this.collection.length;
+        for (var i = 0; i < length; i++) {
+            for (var j = 0; j <= length - i - 1; j++) {
+                if (this.collection[j] > this.collection[j + 1]) {
+                    var leftHand = this.collection[j];
+                    this.collection[j] = this.collection[j + 1];
+                    this.collection[j + 1] = leftHand;
+                }
+            }
+        }
+    };
+    return Sorter;
+}());
+var sorter = new Sorter([10, 3, -5, 0]);
+console.log("before", sorter.collection);
+sorter.sort();
+console.log("after", sorter.collection);
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,31 @@
-console.log('hi there')
+class Sorter {
+  // collection: number[];
 
-const logSomething = () => {
-  console.log('something')
+  // constructor(collection: number[]) {
+  //   this.collection = collection;
+  // }
+  constructor(public collection: number[]) {
+  }
+
+  sort():void {
+    // const length = this.collection.length;
+    const { length } = this.collection;
+
+    for (let i = 0; i < length; i++) {
+      for (let j = 0; j <= length -i -1; j++) {
+        if (this.collection[j] > this.collection[j +1]) {
+          const leftHand = this.collection[j];
+          this.collection[j] = this.collection[j + 1];
+          this.collection[j+1] = leftHand;
+        }
+      }
+    }
+  }
 }
+
+const sorter = new Sorter([10, 3, -5, 0])
+console.log("before", sorter.collection)
+sorter.sort()
+console.log("after", sorter.collection)
+
+
