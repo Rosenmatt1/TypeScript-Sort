b861bfdb65b5448fc741ccd6e8a6a231b0b67f5d
Matthew Rosenberg <Rosenmatt1@gmail.com>
2020-04-01 17:51:03 -0600

    Added a solution for if collection is string or array

--- build/index.js
+++ build/index.js
@@ -7,15 +7,20 @@ var Sorter = /** @class */ (function () {
     function Sorter(collection) {
         this.collection = collection;
     }
+    // What if collection is a string?  Strings are immutable?
     Sorter.prototype.sort = function () {
         // const length = this.collection.length;
         var length = this.collection.length;
         for (var i = 0; i < length; i++) {
             for (var j = 0; j <= length - i - 1; j++) {
-                if (this.collection[j] > this.collection[j + 1]) {
-                    var leftHand = this.collection[j];
-                    this.collection[j] = this.collection[j + 1];
-                    this.collection[j + 1] = leftHand;
+                //All of this only works if collection is nummber[]
+                //Ifcollection is an array of nunbers
+                if (this.collection instanceof Array) {
+                    if (this.collection[j] > this.collection[j + 1]) {
+                        var leftHand = this.collection[j];
+                        this.collection[j] = this.collection[j + 1];
+                        this.collection[j + 1] = leftHand;
+                    }
                 }
             }
         }
--- src/index.ts
+++ src/index.ts
@@ -7,16 +7,23 @@ class Sorter {
   constructor(public collection: number[]) {
   }
 
-  sort():void {
+  // What if collection is a string?  Strings are immutable?
+
+  sort(): void {
     // const length = this.collection.length;
     const { length } = this.collection;
 
     for (let i = 0; i < length; i++) {
-      for (let j = 0; j <= length -i -1; j++) {
-        if (this.collection[j] > this.collection[j +1]) {
-          const leftHand = this.collection[j];
-          this.collection[j] = this.collection[j + 1];
-          this.collection[j+1] = leftHand;
+      for (let j = 0; j <= length - i - 1; j++) {
+
+        //All of this only works if collection is nummber[]
+        //Ifcollection is an array of nunbers
+        if (this.collection instanceof Array) {
+          if (this.collection[j] > this.collection[j + 1]) {
+            const leftHand = this.collection[j];
+            this.collection[j] = this.collection[j + 1];
+            this.collection[j + 1] = leftHand;
+          }
         }
       }
     }
