35f793002450b67fa2d3335cb879022ee146e3dd
Matthew Rosenberg <Rosenmatt1@gmail.com>
2020-04-02 20:57:44 -0600

    Created a CharactersCollection class

--- build/index.js
+++ build/index.js
@@ -1,8 +1,12 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-var NumbersCollection_1 = require("./NumbersCollection");
 var Sorter_1 = require("./Sorter");
+var NumbersCollection_1 = require("./NumbersCollection");
 var numbersCollection = new NumbersCollection_1.NumbersCollection([10, 3, -5, 0]);
 var sorter = new Sorter_1.Sorter(numbersCollection);
 sorter.sort();
 console.log(numbersCollection.data);
+// const charactersCollection = new CharactersCollection('cXas');
+// const sorter = new Sorter(charactersCollection);
+// sorter.sort();
+// console.log(charactersCollection.data)
--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,17 @@
-import { NumbersCollection } from './NumbersCollection';
 import { Sorter } from './Sorter';
+import { NumbersCollection } from './NumbersCollection';
+import { CharactersCollection } from './CharactersCollection'
 
 
 const numbersCollection = new NumbersCollection([10, 3, -5, 0]);
 const sorter = new Sorter(numbersCollection);
 sorter.sort();
 console.log(numbersCollection.data);
+
+// const charactersCollection = new CharactersCollection('cXas');
+// const sorter = new Sorter(charactersCollection);
+// sorter.sort();
+// console.log(charactersCollection.data)
+
 
 
